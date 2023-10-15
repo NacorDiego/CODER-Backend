7097a99fa929de0446b64fea1ba97e1913e84ca6
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2023-10-15 09:45:52 -0300

    Se agrega handlebars. Se agrega directorio 'views'.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
 const app = express();
 const productRoutes = require('./routes/productRoutes.js');
 const cartRoutes = require('./routes/cartRoutes.js');
 const port = 8080;
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 // Middleware para procesar JSON en las solicitudes
 app.use(express.json());
