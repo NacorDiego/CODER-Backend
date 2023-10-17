dd160388b2a9b29d882b6fb203894bd25c1124a1
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2023-10-16 23:13:47 -0300

    Se agrega configuración de socket para el lado del cliente y del servidor. Se agregan scripts de socket e index en vista index.

--- src/app.js
+++ src/app.js
@@ -4,12 +4,15 @@ import cartRoutes from './routes/cartRoutes.js';
 import viewsRouter from './routes/views.router.js';
 import handlebars from 'express-handlebars';
 import __dirname from './utils.js';
+import { Server } from 'socket.io';
 
 const port = 8080;
 const app = express();
 const httpServer = app.listen(port, "localhost", () => console.log(`Servidor Express en ejecución en http://localhost:${port}`));
 
+// Instancio un socket del lado del servidor
 const socketServer = new Server(httpServer);
+
 // Inicializamos el motor indicando con app.engine('qué motor utilizaremos', el motor instanciado)
 app.engine('handlebars', handlebars.engine());
 // Indicamos en qué parte estarán las rutas
@@ -30,6 +33,13 @@ app.use('/api/carts', cartRoutes);
 // La lógica de las vistas queda en el router de vistas
 app.use('/', viewsRouter);
 
-// app.listen(port,"localhost", () => {
-//     console.log(`Servidor Express en ejecución en http://localhost:${port}`);
-// });
+socketServer.on('connection', socket => {
+    console.log('Nuevo cliente conectado');
+
+    // Dejo al socket escuchando hasta que el cliente mande un msj o evento al ID 'message'
+    socket.on('message', data => {
+        console.log(data);
+    });
+});
+
+// pag 50
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,1 +1,5 @@
-const saludar = () => alert("Hola mundo");
+// Instancio socket del lado del cliente
+const socket = io();
+
+// Envio un mensaje desde el socket del cliente al servidor, con el ID 'message'
+socket.emit('message', 'Hola, estoy me estoy comunicando desde el socket del cliente!');
