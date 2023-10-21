5d514391537c09b748b989ed6cb6d87784a63bbe
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2023-10-21 13:05:47 -0300

    Muestro los productos en home.handlebars

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express'
+import ProductManager from '../managers/ProductManager.js'
 
 const router = Router()
 
+const productManager = new ProductManager(process.cwd() + '/src/data/productos.json')
+
 router.get('/', (req, res) => {
-    res.render('index', {})
+    const products = productManager.getProducts()
+    console.log('Productos cargados:', products)
+    res.render('home', { products })
 })
 
 export { router }
