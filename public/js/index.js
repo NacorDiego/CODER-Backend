8da766536723fa1183fc018613d6d71fb8fc7973
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2023-11-09 13:39:30 -0300

    Cambios menores.

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,11 @@
 // Instancio socket del lado del cliente
 const socket = io()
 
-// Envio un mensaje desde el socket del cliente al servidor, con el ID 'message'
-socket.emit('message', 'Hola, estoy me estoy comunicando desde el socket del cliente!')
+window.addEventListener('DOMContentLoaded', _event => {
+    socket.on('greet', data => {
+        const output = document.querySelector('#output')
+        output.innerHTML += `<li>${data}</li>`
+    })
 
-socket.on('evento_para_socket_individual', data => {
-    console.log(data)
-})
-
-socket.on('evento_para_todos_menos_el_actual', data => {
-    console.log(data)
-})
-
-socket.on('evento_para_todos', data => {
-    console.log(data)
+    socket.emit('message', 'Hola, me estoy comunicando desde un webSocket!!!')
 })
